f15fc68e5828af0f2f104c09e6022ec58196c11d
MdRokibulHasan <mdrokibulhasan42@gmail.com>
2021-11-23 23:17:00 +0600

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import '../App.css';
 import React from "react";
+import { Button } from 'antd';
 
 // import Banner from '../assets/images/bannar.jpg'
 import TopBarBanner from "../Components/TopBanner/TopBarBanner";
@@ -7,6 +8,8 @@ const Home = () => {
     return (
         <div>
             <TopBarBanner />
+
+            <Button type="primary">DaDA Dora khaice</Button>
         </div>
     );
 }
